490e11816b96cc34512872942a33a4e7681551e7
aaptrv304-tech <aaptrv304@gmail.com>
2026-02-02 02:29:40 +0300

    Its alive !

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Конфигурация API
 export const API_CONFIG = {
     // Твой ngrok URL
-    baseURL: 'https://warcraft-evaluation-yes-whats.trycloudflare.com',
+    baseURL: 'https://cleveland-cant-jpeg-initiative.trycloudflare.com',
     endpoints: {
         businesses: '/api/businesses',
         activity: '/api/activity',
