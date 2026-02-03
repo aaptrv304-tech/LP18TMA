d8c690dac2667dffafa4c8548c7ff3949ad9676b
aaptrv304-tech <aaptrv304@gmail.com>
2026-02-03 12:15:40 +0300

    Server up

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Конфигурация API
 export const API_CONFIG = {
     // Твой ngrok URL
-    baseURL: 'https://cir-share-watch-less.trycloudflare.com ',
+    baseURL: 'https://cir-share-watch-less.trycloudflare.com',
     endpoints: {
         businesses: '/api/businesses',
         activity: '/api/activity',
