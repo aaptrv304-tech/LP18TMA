0a4c6b44de4803e46dfc92ef7662c7fc6a1045f6
aaptrv304-tech <aaptrv304@gmail.com>
2026-02-06 14:38:53 +0300

    Server up

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Конфигурация API
 export const API_CONFIG = {
     // Твой ngrok URL
-    baseURL: 'https://deaf-bathrooms--forgotten.trycloudflare.com',
+    baseURL: 'https://lucky-solomon-returns-approach.trycloudflare.com',
     endpoints: {
         businesses: '/api/businesses',
         activity: '/api/activity',
