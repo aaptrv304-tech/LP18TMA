7a755a2a42227141c4639a376a577534d7c89999
aaptrv304-tech <aaptrv304@gmail.com>
2026-02-11 14:04:15 +0300

    Server up

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Конфигурация API
 export const API_CONFIG = {
     // Твой ngrok URL
-    baseURL: 'https://proud-karen-pest-damages.trycloudflare.com',
+    baseURL: 'https://harley-receiver-accounts-containers.trycloudflare.com',
     endpoints: {
         businesses: '/api/businesses',
         activity: '/api/activity',
