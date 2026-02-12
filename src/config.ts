5d646b37a3e5912adc47cb0329eb8de8516d6541
aaptrv304-tech <aaptrv304@gmail.com>
2026-02-12 17:20:05 +0300

    Server up

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Конфигурация API
 export const API_CONFIG = {
     // Твой ngrok URL
-    baseURL: 'https://moderate-counties-bite-celebrity.trycloudflare.com',
+    baseURL: 'https://pediatric-salt-instantly-watershed.trycloudflare.com',
     endpoints: {
         businesses: '/api/businesses',
         activity: '/api/activity',
